dbf9fd17350c4e9aff1d8412529431faa6898a55
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-03-04 15:27:41 +0600

    CRUD api created

--- src/app/controller/books_routes.ts
+++ src/app/controller/books_routes.ts
@@ -16,14 +16,48 @@ booksRoutes.get('/', async (req: Request, res: Response) => {
     })
 })
 
-booksRoutes.post('/create-book', async (req: Request, res: Response) => {
+booksRoutes.post('/', async (req: Request, res: Response) => {
     const body = req.body;
-    const myBook = await Books.create(body)
+    const createdBook = await Books.create(body)
 
     res.json({
         success: true,
         message: "Book created successfully",
-        data: myBook
+        data: createdBook
     })
 
 })
+
+
+booksRoutes.get('/:bookId', async (req: Request, res: Response) => {
+    const bookId = req.params.bookId
+    const book = await Books.findById(bookId)
+    res.json({
+        success: true,
+        message: "Book retrived successfully",
+        data: book 
+    })
+})
+booksRoutes.patch('/:bookId', async (req: Request, res: Response) => {
+    const bookId = req.params.bookId
+    const updateBody = req.body
+    const updatedBook = await Books.findByIdAndUpdate(bookId, updateBody, {new:true} )
+    res.json({
+        success: true,
+        message: "Book updated successfully",
+        data: updatedBook
+    })
+})
+
+
+booksRoutes.delete('/:bookId', async (req: Request, res: Response) => {
+    const bookId = req.params.bookId
+    const deletedBook = await Books.findByIdAndDelete(bookId)
+    res.json({
+        success: true,
+        message: "Book retrived successfully",
+        data: deletedBook
+    })
+})
+
+
